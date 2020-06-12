6f4921d3498daba75ee49bd0a4cf5f307d1945cc
Yunqi <857560476@qq.com>
2020-06-12 21:31:49 +0800

    组合继承

--- 算法/leetcode/array类型/leetcode_01.js
+++ 算法/leetcode/array类型/leetcode_01.js
@@ -2,11 +2,23 @@
 
 var arr = [3, 2,4]
 function twoSum(arr, target) {
-  for(let i = 0; i< arr.length; i++) {
-    if(arr.slice(i+1).indexOf(target - arr[i]) > -1) {
-      console.log(arr.slice(i+1))
-      return [arr.indexOf(target - arr[i]), i]
+  // for(let i = 0; i< arr.length; i++) {
+  //   if(arr.slice(i+1).indexOf(target - arr[i]) > -1) {
+  //     console.log(arr.slice(i+1))
+  //     return [arr.indexOf(target - arr[i]), i]
+  //   }
+  // }
+  var value = new Map()
+  for (let i =0; i<arr.length; i++) {
+    // sum：3 
+    const sum  = target - arr[i] 
+    const matchValue = value.get(sum)
+    console.log( value,matchValue)
+    if(matchValue !== undefined) {
+      return [matchValue, i]
     }
+    value.set(arr[i], i)
+    console.log(value)
   }
 }
 
