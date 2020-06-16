9324727ffe2b06bf15f497993fb69f7a487b2e4b
Yunqi <857560476@qq.com>
2020-06-16 22:15:04 +0800

    合并push

--- 跨域/server.js
+++ 跨域/server.js
@@ -1,11 +1,6 @@
 var http = require('http')
 
 http.createServer(function(req, res) {
-  // res.write({
-  //   code:0,
-  //   name:'蜗牛',
-  //   age:18
-  // })
   // res.write('<head><meta charset="UTF-8"></head>')
   res.write('服务器启动成功')
   res.end()
