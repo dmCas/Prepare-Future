1b2883a9ffd7798c712430323ff47bf8f2bcd0a2
Yunqi <857560476@qq.com>
2020-06-26 22:33:29 +0800

    浏览器性能优化

--- 面向对象/definedProperty/definedProperty.js
+++ 面向对象/definedProperty/definedProperty.js
@@ -7,11 +7,12 @@ Object.defineProperty(person, "name", {
   value:'Beichen',
   // writable:true
 })
-console.log(person._sex)
-delete person.name
-console.log(person)
+// console.log(person._sex)
+// delete person.name
+// console.log(person)
 
 for(let k of Object.keys(person)) {
+  console.log(Object.keys(person))
   console.log(person[k])
 }
 
