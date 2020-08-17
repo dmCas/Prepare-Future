5293609d570f0d771603c2f86cb6b15a8e451eb2
jiangjun06 <jiangjun06@baidu.com>
2020-08-17 22:01:19 +0800

    update

--- React_知识点/React-router/hash-router/src/UserAdd.jsx
+++ React_知识点/React-router/hash-router/src/UserAdd.jsx
@@ -1,10 +1,30 @@
 import React, { Component } from 'react'
 
 export default class UserAdd extends Component {
+    constructor() {
+        super()
+        this.state = {
+            arr: [1, 1, 3]
+        }
+    }
+
+
+
     render() {
         return (
             <div>
-                <p>123</p>UserAdd
+                <ul>
+                    {
+                        this.state.arr.map((item, index) => <li key={item}>{item}</li>)
+                    }
+                </ul>
+                <button onClick={() => {
+                    this.setState({
+                        arr: this.state.arr.reverse()
+                    })
+                }}>
+                    改变
+                </button>
             </div>
         )
     }
